2d91d86080ca4b6b8f1a0499ffe5e68576cb256f
ma.nam <maianhnamdev@gmail.com>
2022-11-02 16:25:25 +0700

    cscs

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,14 +1,23 @@
 
 import React from "react";
 import "./Widget.css"
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 function Widget() {
     return (
         <div className="Widget">
             <div className="left">
-                <span className="title"></span>
+                <span className="title">User</span>
+                <span className="counter">34353</span>
+                <span className="link">Xem chi tiết</span>
             </div>
-            <div className="right">right</div>
+            <div className="right">
+                <div className="percentage positive">
+                    <KeyboardArrowUpIcon />
+                    20%</div>
+            </div>
+            <PersonOutlineIcon className="icon"/>
         </div>
     );
 }
